5ea620b75776556d82c696d0079519e1ce9dfccd
dgarcia360 <dgarcia360@outlook.com>
2021-01-22 11:09:04 +0000

    up

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,6 @@
 import os
 import re
 import sys
-import subprocess
 import yaml
 from datetime import date
 from docutils import nodes
@@ -205,7 +204,7 @@ redirects_file = "_utils/redirections.yaml"
 TAGS = []
 smv_tag_whitelist = multiversion_regex_builder(TAGS)
 # Whitelist pattern for branches (set to None to ignore all branches)
-BRANCHES = ['master']
+BRANCHES = ['added-reference-generator']
 smv_branch_whitelist = multiversion_regex_builder(BRANCHES)
 # Whitelist pattern for remotes (set to None to use local branches only)
 smv_remote_whitelist = r"^origin$"
